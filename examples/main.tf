b57acf7ba9a2e1a935ff4af2f2ed9a2562c87d30
yoshutch <yoshutch@gmail.com>
2019-12-05 13:09:41 -0700

    added version definition in the module usage example removed unnecessary comment

--- examples/main.tf
+++ examples/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 module "acs" {
   source = "../"
+  version = "1.0.0"
   env = "dev"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
 
-//variable "vpc_name" {
-//  type = string
-//  default = "oit-oregon-dev"
-//  description = "VPC name. [oit-oregon-dev, oit-oregon-prd, oit-oregon-dev, oit-oregon-prd, oit-virginia-dev, oit-virginia-prd, oit-virginia-dev, oit-virginia-prd]"
-//}
-
 variable "env" {
   type = string
   description = "Account environment (e.g. dev, prd)"
