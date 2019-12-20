296d5785854388e04a60a95fac199fcca0691a66
Josh Gubler <josh.gubler@gmail.com>
2019-12-20 11:09:50 -0700

    output oracle security group

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 
 variable "env" {
-  type = string
+  type        = string
   description = "Account environment (e.g. dev, prd)"
 }
 
 variable "vpc_vpn_to_campus" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Retrieve VPC info for the VPC that has VPN access to campus (defaults to false)."
 }
