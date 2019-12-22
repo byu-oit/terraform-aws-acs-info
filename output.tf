783588b22681e45231c38e022f643abd0598f30e
Josh Gubler <josh.gubler@gmail.com>
2019-12-21 20:53:50 -0700

    handle missing required resources

--- output.tf
+++ output.tf
@@ -6,10 +6,10 @@ output "power_builder_role" {
   value = data.aws_iam_role.power_builder
 }
 output "role_permissions_boundary" {
-  value = data.aws_iam_policy.role_permission_boundary
+  value = local.role_permission_boundary_arn != null ? data.aws_iam_policy.role_permission_boundary[0] : null
 }
 output "user_permissions_boundary" {
-  value = data.aws_iam_policy.user_permission_boundary
+  value = local.user_permission_boundary_arn != null ? data.aws_iam_policy.user_permission_boundary[0] : null
 }
 
 // VPC outputs
@@ -17,30 +17,30 @@ output "vpc" {
   value = data.aws_vpc.vpc
 }
 output "private_subnet_ids" {
-  value = [data.aws_subnet.private_a.id, data.aws_subnet.private_b.id]
+  value = local.private_a_subnet_id != null ? [data.aws_subnet.private_a[0].id, data.aws_subnet.private_b[0].id] : null
 }
 output "public_subnet_ids" {
-  value = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id]
+  value = local.public_a_subnet_id != null ? [data.aws_subnet.public_a[0].id, data.aws_subnet.public_b[0].id] : null
 }
 output "data_subnet_ids" {
-  value = [data.aws_subnet.data_a.id, data.aws_subnet.data_b.id]
+  value = local.data_a_subnet_id != null ? [data.aws_subnet.data_a[0].id, data.aws_subnet.data_b[0].id] : null
 }
 output "private_subnets" {
-  value = [data.aws_subnet.private_a, data.aws_subnet.private_b]
+  value = local.private_a_subnet_id != null ? [data.aws_subnet.private_a[0], data.aws_subnet.private_b[0]] : null
 }
 output "public_subnets" {
-  value = [data.aws_subnet.public_a, data.aws_subnet.public_b]
+  value = local.public_a_subnet_id != null ? [data.aws_subnet.public_a[0], data.aws_subnet.public_b[0]] : null
 }
 output "data_subnets" {
-  value = [data.aws_subnet.data_a, data.aws_subnet.data_b]
+  value = local.data_a_subnet_id != null ? [data.aws_subnet.data_a[0], data.aws_subnet.data_b[0]] : null
 }
 
 // DNS outputs
 output "route53_zone" {
-  value = data.aws_route53_zone.zone
+  value = local.zone_id != null ? data.aws_route53_zone.zone[0] : null
 }
 output "certificate" {
-  value = data.aws_acm_certificate.cert
+  value = local.zone_id != null ? data.aws_acm_certificate.cert[0] : null
 }
 
 // RDS Outputs
@@ -59,10 +59,13 @@ output "rds_security_group" {
 }
 
 output "oracle_security_group" {
-  value = local.has_oracle_security_group ? data.aws_security_group.oracle[0] : null
+  value = local.oracle_security_group_id != null ? data.aws_security_group.oracle[0] : null
 }
 
 // CodePipeline outputs
 output "github_token" {
+  // We just pull a simple string from acs_parameters. Because there is no data source,
+  // we don't need the protection offered by our count() pattern. In this case, a
+  // lookup() gives us the same protection and is simpler.
   value = lookup(local.acs_info, "/acs/git/token", null)
 }
