ffe171f652fa399f860c8bd9e009577559efd2e6
Josh Gubler <josh.gubler@gmail.com>
2020-07-07 14:06:14 -0600

    update example to use v2.1.0

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "acs" {
   //  source = "../../"
-  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v2.0.0"
+  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v2.1.0"
 }
 
 output "vpc_id" {
