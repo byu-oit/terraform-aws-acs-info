55532a09330259e5802c7dd3db0e577598b3873d
Josh Gubler <jgubler@byu.edu>
2024-09-09 14:46:03 -0600

    fix sensitive outputs in example

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -11,7 +11,8 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  value = module.acs.private_subnet_ids
+  value     = module.acs.private_subnet_ids
+  sensitive = true
 }
 
 output "power_builder_role_arn" {
@@ -23,7 +24,8 @@ output "power_builder_policy_arns" {
 }
 
 output "permission_boundary" {
-  value = module.acs.role_permissions_boundary.arn
+  value     = module.acs.role_permissions_boundary.arn
+  sensitive = true
 }
 
 output "route53_zone_name" {
@@ -39,20 +41,26 @@ output "cert_arn" {
 }
 
 output "github_oidc_arn" {
-  value = module.acs.github_oidc_provider.arn
+  value     = module.acs.github_oidc_provider.arn
+  sensitive = true
 }
 output "github_token" {
-  value = module.acs.github_token
+  value     = module.acs.github_token
+  sensitive = true
 }
 output "humio_dev_token" {
-  value = module.acs.humio_dev_token
+  value     = module.acs.humio_dev_token
+  sensitive = true
 }
 output "humio_prd_token" {
-  value = module.acs.humio_prd_token
+  value     = module.acs.humio_prd_token
+  sensitive = true
 }
 output "humio_dev_endpoint" {
-  value = module.acs.humio_dev_endpoint
+  value     = module.acs.humio_dev_endpoint
+  sensitive = true
 }
 output "humio_prd_endpoint" {
-  value = module.acs.humio_prd_endpoint
+  value     = module.acs.humio_prd_endpoint
+  sensitive = true
 }
